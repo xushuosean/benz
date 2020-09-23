037e48c41f6112dfc93acb2647eda33cd4d7bda8
sean <1963966513@qq.com>
2020-09-23 18:18:00 +0800

    change1

--- src/views/change.vue
+++ src/views/change.vue
@@ -6,7 +6,7 @@
           <p>韩宇:</p>
           <p class="text">你好。欢迎加入恒星。今天是你入职的第一天，由于我出差在外，不能进行面谈，但有个紧急事情需要你替我处理一下。以下是今年第一季度的运营数据，我希望你对数据进行分析，并将看到的问题、可能产生的原因以及后续如何改善形成详细的方案发送至我邮箱。</p>
           <el-image
-            src="../assets/pic1.png"
+            src="https://xushuosean.github.io/benz/img/pic1.a7d1d48e.png"
             :preview-src-list="srcList">
           </el-image>
           <img src="../assets/pic1.png" alt="">
@@ -34,7 +34,7 @@ export default {
       from: '来自运营总监杜子夫的邮件',
       textarea: '',
       srcList: [
-        '../assets/pic1.png'
+        'https://xushuosean.github.io/benz/img/pic1.a7d1d48e.png'
       ]
     }
   },
