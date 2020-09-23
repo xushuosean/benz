32963153e07d6f0669348a7386544288196d2d85
sean <1963966513@qq.com>
2020-09-23 16:09:32 +0800

    更换pic1

--- src/views/change.vue
+++ src/views/change.vue
@@ -7,7 +7,7 @@
           <p class="text">你好。欢迎加入恒星。今天是你入职的第一天，由于我出差在外，不能进行面谈，但有个紧急事情需要你替我处理一下。以下是今年第一季度的运营数据，我希望你对数据进行分析，并将看到的问题、可能产生的原因以及后续如何改善形成详细的方案发送至我邮箱。</p>
           <el-image
             style="width: 100px; height: 100px"
-            src="https://xushuosean.github.io/benz/img/pic1.a7d1d48e.png"
+            src="../assets/pic1.png"
             :preview-src-list="srcList">
           </el-image>
           <el-audio audioLink="../assets/hello.m4a">
