8ed2837666949fe7bdf8614dd282f0f7adab8fb6
sean <1963966513@qq.com>
2020-11-12 13:45:49 +0800

    deploy

---
+++ precache-manifest.3684a619ff9210d8f881a69ff997e40f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686148797db3aa2f65c3",
+    "url": "/benz/css/app.6830f339.css"
+  },
+  {
+    "revision": "29bba4d2eefeac8b0998",
+    "url": "/benz/css/chunk-2c88d9d5.780f82e7.css"
+  },
+  {
+    "revision": "2b3d718b6787ef45b210",
+    "url": "/benz/css/chunk-3872554f.9976fb9d.css"
+  },
+  {
+    "revision": "9d792f6b053d94f6f61e",
+    "url": "/benz/css/chunk-5d6d4958.9bd646d0.css"
+  },
+  {
+    "revision": "4de5ba79f5271fb04307",
+    "url": "/benz/css/chunk-5db8b97a.571fc946.css"
+  },
+  {
+    "revision": "03f667c668b837b28b79",
+    "url": "/benz/css/chunk-718207c8.eb4e115d.css"
+  },
+  {
+    "revision": "3e5d07d5f27f57354725",
+    "url": "/benz/css/chunk-875857be.25d559a8.css"
+  },
+  {
+    "revision": "564eac50576c19739011",
+    "url": "/benz/css/chunk-vendors.9320e428.css"
+  },
+  {
+    "revision": "46ac6629168546c71dbf115992aa4be9",
+    "url": "/benz/fonts/VideoJS.46ac6629.eot"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/benz/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/benz/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "c96e3b7689489ebe72d8c84d680636e3",
+    "url": "/benz/img/backimg.c96e3b76.png"
+  },
+  {
+    "revision": "3c0b2e7130669da7a813e28c7c7730e4",
+    "url": "/benz/img/logo.3c0b2e71.png"
+  },
+  {
+    "revision": "d169593f6d6e20b2da69d5e8879f1f19",
+    "url": "/benz/img/person1.d169593f.png"
+  },
+  {
+    "revision": "a7d1d48e1ad7faeeb09b2f79a3238525",
+    "url": "/benz/img/pic1.a7d1d48e.png"
+  },
+  {
+    "revision": "3bae486895fd2e4c585076eb348d1eee",
+    "url": "/benz/img/play.3bae4868.png"
+  },
+  {
+    "revision": "cd602c7524cc8fe0216c37baacd59ca3",
+    "url": "/benz/img/stop.cd602c75.png"
+  },
+  {
+    "revision": "3b502418687371e71518b4cbd7d1a19c",
+    "url": "/benz/index.html"
+  },
+  {
+    "revision": "65e013ce0c1e548db778",
+    "url": "/benz/js/about.cb7b6e3e.js"
+  },
+  {
+    "revision": "686148797db3aa2f65c3",
+    "url": "/benz/js/app.16d7b1f4.js"
+  },
+  {
+    "revision": "29bba4d2eefeac8b0998",
+    "url": "/benz/js/chunk-2c88d9d5.0cddd719.js"
+  },
+  {
+    "revision": "2b3d718b6787ef45b210",
+    "url": "/benz/js/chunk-3872554f.16cb793b.js"
+  },
+  {
+    "revision": "9d792f6b053d94f6f61e",
+    "url": "/benz/js/chunk-5d6d4958.4db1b570.js"
+  },
+  {
+    "revision": "4de5ba79f5271fb04307",
+    "url": "/benz/js/chunk-5db8b97a.bb5aa7d1.js"
+  },
+  {
+    "revision": "03f667c668b837b28b79",
+    "url": "/benz/js/chunk-718207c8.af58bdf1.js"
+  },
+  {
+    "revision": "3e5d07d5f27f57354725",
+    "url": "/benz/js/chunk-875857be.57a77b01.js"
+  },
+  {
+    "revision": "564eac50576c19739011",
+    "url": "/benz/js/chunk-vendors.a7bc0855.js"
+  },
+  {
+    "revision": "5d63c1cb9e2e332efee3b69791529a7d",
+    "url": "/benz/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/benz/robots.txt"
+  }
+]);
